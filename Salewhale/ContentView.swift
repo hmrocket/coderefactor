61702a3a3001ee534de76b6f005df22be819ad75
Mhamed Hlioui <Mhamed.hlioui@bell.ca>
2020-07-03 00:03:05 -0400

    Initial Commit

---
+++ Salewhale/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Salewhale
+//
+//  Created by Mhamed on 2020-07-03.
+//  Copyright © 2020 Salewhale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
